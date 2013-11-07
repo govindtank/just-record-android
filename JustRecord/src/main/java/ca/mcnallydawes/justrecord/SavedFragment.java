990de7b6d788034a31a6c886734f909322c6f6f1
unknown <H100173@HPSBC358BBKCP1.dev1.honeywell.com>
2013-11-07 14:16:18 -0800

    I don't remember what's different, saved recordings now show date and size, sorting works, recording works but I think that was in the last commit

--- JustRecord/src/main/java/ca/mcnallydawes/justrecord/SavedFragment.java
+++ JustRecord/src/main/java/ca/mcnallydawes/justrecord/SavedFragment.java
@@ -1,16 +1,24 @@
 package ca.mcnallydawes.justrecord;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -21,7 +29,9 @@ public class SavedFragment extends Fragment {
     private List<Button> mFilterButtons;
     private ListView mListView;
     private SavedRecordingsAdapter mAdapter;
-    private ArrayList<String> mData;
+    private ArrayList<File> mData;
+    private MediaPlayer mPlayer;
+    private int mClickedItem = -1;
 
     public static SavedFragment newInstance() {
         return new SavedFragment();
@@ -40,11 +50,33 @@ public class SavedFragment extends Fragment {
         mFilterButtons.add((Button) rootView.findViewById(R.id.saved_button_filter_length));
         mFilterButtons.add((Button) rootView.findViewById(R.id.saved_button_filter_size));
 
-        for(Button btn : mFilterButtons) {
+        for(int i = 0; i < mFilterButtons.size(); i++) {
+            Button btn = mFilterButtons.get(i);
+            final int index = i;
             btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     savedFilterButtonTap(view);
+                    switch(index) {
+                        case 0:
+                            sortByDateModified();
+                            mAdapter.notifyDataSetChanged();
+                            break;
+                        case 1:
+                            sortByName();
+                            mAdapter.notifyDataSetChanged();
+                            break;
+                        case 2:
+                            sortByLength();
+                            mAdapter.notifyDataSetChanged();
+                            break;
+                        case 3:
+                            sortBySize();
+                            mAdapter.notifyDataSetChanged();
+                            break;
+                        default:
+                            break;
+                    }
                 }
             });
         }
@@ -53,9 +85,67 @@ public class SavedFragment extends Fragment {
 
         mListView = (ListView) rootView.findViewById(R.id.saved_listView_list);
         mData = getListFiles(new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/JustRecord"));
+        sortByDateModified();
+
         mAdapter = new SavedRecordingsAdapter(getActivity(), R.layout.list_item_saved, mData);
 
         mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(mClickedItem == i) {
+                    mPlayer.stop();
+                    mPlayer.release();
+                    mPlayer = null;
+                } else {
+                    if(mPlayer != null) {
+                        mPlayer.release();
+                        mPlayer = null;
+                    }
+
+                    mPlayer = new MediaPlayer();
+                    mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+                    try {
+                        mPlayer.setDataSource(Environment.getExternalStorageDirectory().getAbsolutePath() + "/JustRecord/" + mData.get(i));
+                        mPlayer.prepare();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    mPlayer.start();
+                    mClickedItem = i;
+                }
+            }
+        });
+
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final int index = i;
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Delete")
+                        .setMessage("Are you sure you want to delete " + mData.get(i) + "?")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/JustRecord/" + mData.get(index)).delete();
+                                refreshListView();
+                                dialogInterface.cancel();
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                builder.create().show();
+                return false;
+            }
+        });
 
         return rootView;
     }
@@ -65,20 +155,61 @@ public class SavedFragment extends Fragment {
             item.setSelected(false);
         }
         btn.setSelected(true);
+
+
     }
 
-    private ArrayList<String> getListFiles(File parentDir) {
-        ArrayList<String> inFiles = new ArrayList<String>();
+    private ArrayList<File> getListFiles(File parentDir) {
+        ArrayList<File> inFiles = new ArrayList<File>();
         File[] files = parentDir.listFiles();
         for (File file : files) {
             if (!file.isDirectory()) {
-                    inFiles.add(file.getName());
+                inFiles.add(file);
             }
         }
         return inFiles;
     }
 
     public void refreshListView() {
+        mData.clear();
+        ArrayList<File> fileNames = getListFiles(new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/JustRecord"));
+        for(File file : fileNames) {
+            mData.add(file);
+        }
 
+        sortByDateModified();
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void sortByDateModified() {
+        Collections.sort(mData, new Comparator<File>() {
+            public int compare(File a, File b) {
+                if(a.lastModified() > b.lastModified()) return 1;
+                else if(a.lastModified() < b.lastModified()) return -1;
+                else return 0;
+            }
+        });
+    }
+
+    private void sortByName() {
+        Collections.sort(mData, new Comparator<File>() {
+            public int compare(File a, File b) {
+                return a.getName().compareTo(b.getName());
+            }
+        });
+    }
+
+    private void sortByLength() {
+
+    }
+
+    private void sortBySize() {
+        Collections.sort(mData, new Comparator<File>() {
+            public int compare(File a, File b) {
+                if(a.length() > b.length()) return 1;
+                else if(a.length() < b.length()) return -1;
+                else return 0;
+            }
+        });
     }
 }
--- JustRecord/src/main/java/ca/mcnallydawes/justrecord/SavedRecordingsAdapter.java
+++ JustRecord/src/main/java/ca/mcnallydawes/justrecord/SavedRecordingsAdapter.java
@@ -7,19 +7,23 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by H100173 on 11/7/13.
  */
-public class SavedRecordingsAdapter extends ArrayAdapter<String> {
+public class SavedRecordingsAdapter extends ArrayAdapter<File> {
 
     private Activity mContext;
     private int mLayoutResourceId;
-    private ArrayList<String> mData;
+    private ArrayList<File> mData;
 
-    public SavedRecordingsAdapter(Activity context, int layoutResourceId, ArrayList<String> objects) {
+    public SavedRecordingsAdapter(Activity context, int layoutResourceId, ArrayList<File> objects) {
         super(context, layoutResourceId, objects);
         mContext = context;
         mLayoutResourceId = layoutResourceId;
@@ -35,17 +39,36 @@ public class SavedRecordingsAdapter extends ArrayAdapter<String> {
             convertView = inflater.inflate(mLayoutResourceId, parent, false);
             holder = new ViewHolder();
             holder.nameTV = (TextView) convertView.findViewById(R.id.saved_list_item_textView_name);
+            holder.dateTV = (TextView) convertView.findViewById(R.id.saved_list_item_textView_date);
+//            holder.durationTV = (TextView) convertView.findViewById(R.id.saved_list_item_textView_duration);
+            holder.sizeTV = (TextView) convertView.findViewById(R.id.saved_list_item_textView_size);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
 
-        holder.nameTV.setText(mData.get(position));
+        holder.nameTV.setText(mData.get(position).getName());
+        holder.dateTV.setText(new SimpleDateFormat("dd/MM/yyyy, hh:mm aa").format(new Date(mData.get(position).lastModified())));
+//        holder.durationTV.setText("");
+        holder.sizeTV.setText(getFileSize(mData.get(position).length()));
 
         return convertView;
     }
 
+    private String getFileSize(long length) {
+        if(length < 1024) {
+            return String.valueOf(length) + " B";
+        } else if(length < 1048576) {
+            return String.valueOf(length / 1024) + " KB";
+        } else {
+            return String.valueOf(length / 1048576) + " MB";
+        }
+    }
+
     public static class ViewHolder {
         public TextView nameTV;
+        public TextView dateTV;
+        public TextView durationTV;
+        public TextView sizeTV;
     }
 }
