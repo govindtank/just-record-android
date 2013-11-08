90e6808c66444d1d08e32d966ae080a5d70d6cdb
unknown <jeffmcnallydawes@gmail.com>
2013-11-07 22:12:09 -0800

    added CAB icons, playback works now, no actual effect when you select a CAB item

--- JustRecord/src/main/java/ca/mcnallydawes/justrecord/SavedFragment.java
+++ JustRecord/src/main/java/ca/mcnallydawes/justrecord/SavedFragment.java
@@ -7,6 +7,7 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -31,11 +32,16 @@ import java.util.List;
  */
 public class SavedFragment extends Fragment {
 
+    public enum MediaPlayerState {
+        STOPPED, PLAYING, PAUSED
+    }
+
     private List<Button> mFilterButtons;
     private ListView mListView;
     private SavedRecordingsAdapter mAdapter;
     private ArrayList<File> mData;
     private MediaPlayer mPlayer;
+    private MediaPlayerState mPlayerState;
     private int mClickedItem = -1;
 
     public static SavedFragment newInstance() {
@@ -44,9 +50,13 @@ public class SavedFragment extends Fragment {
 
     public SavedFragment() {}
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_saved, container, false);
+
+        mPlayerState = MediaPlayerState.STOPPED;
 
         mFilterButtons = new ArrayList<Button>();
 
@@ -72,7 +82,8 @@ public class SavedFragment extends Fragment {
                             mAdapter.notifyDataSetChanged();
                             break;
                         case 2:
-                            sortByLength();
+//                            sortByLength();
+                            sortByFileType();
                             mAdapter.notifyDataSetChanged();
                             break;
                         case 3:
@@ -90,6 +101,7 @@ public class SavedFragment extends Fragment {
 
         mListView = (ListView) rootView.findViewById(R.id.saved_listView_list);
         mData = getListFiles(new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/JustRecord"));
+
         sortByDateModified();
 
         mAdapter = new SavedRecordingsAdapter(getActivity(), R.layout.list_item_saved, mData);
@@ -99,99 +111,137 @@ public class SavedFragment extends Fragment {
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if(mClickedItem == i) {
-                    mPlayer.stop();
-                    mPlayer.release();
-                    mPlayer = null;
-                } else {
-                    if(mPlayer != null) {
-                        mPlayer.release();
-                        mPlayer = null;
-                    }
-
-                    mPlayer = new MediaPlayer();
-                    mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-                    try {
-                        mPlayer.setDataSource(Environment.getExternalStorageDirectory().getAbsolutePath() + "/JustRecord/" + mData.get(i));
-                        mPlayer.prepare();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    mPlayer.start();
-                    mClickedItem = i;
+                if(mClickedItem == i && mPlayerState == MediaPlayerState.PLAYING) {
+                    pausePlayback();
+                    Log.d("playback", "PAUSE FROM PLAY");
+                } else if(mClickedItem == i && mPlayerState == MediaPlayerState.PAUSED) {
+                    resumePlayback();
+                    Log.d("playback", "PLAY FROM PAUSE");
+                } else if(mClickedItem == i && mPlayerState == MediaPlayerState.STOPPED) {
+                    playFile(mData.get(i));
+                    Log.d("playback", "PLAY FROM STOPPED");
+                } else if(mClickedItem != i) {
+                    stopPlayback();
+                    playFile(mData.get(i));
+                    Log.d("playback", "PLAY NEW");
                 }
+                mClickedItem = i;
             }
         });
 
-//        mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
-//        mListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+        mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        mListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
+
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                MenuInflater inflater = actionMode.getMenuInflater();
+                inflater.inflate(R.menu.saved, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.saved_action_delete:
+                        return false;
+                    case R.id.saved_action_edit:
+                        return false;
+                    case R.id.saved_action_share:
+                        return false;
+                    default:
+                        return false;
+                }
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        });
+
+//        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 //            @Override
-//            public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
-//
-//            }
-//
-//            @Override
-//            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
-//                MenuInflater inflater = actionMode.getMenuInflater();
-//                inflater.inflate(R.menu.saved, menu);
+//            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                final int index = i;
+//                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//                builder.setTitle("Delete")
+//                        .setMessage("Are you sure you want to delete " + mData.get(i) + "?")
+//                        .setCancelable(false)
+//                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/JustRecord/" + mData.get(index)).delete();
+//                                refreshListView();
+//                                dialogInterface.cancel();
+//                            }
+//                        })
+//                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                dialogInterface.cancel();
+//                            }
+//                        });
+//                builder.create().show();
 //                return true;
-//            }
-//
-//            @Override
-//            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
-//                return false;
-//            }
-//
-//            @Override
-//            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
-//                switch (menuItem.getItemId()) {
-//                    case R.id.saved_action_delete:
-//                        return true;
-//                    case R.id.saved_action_edit:
-//                        return true;
-//                    case R.id.saved_action_settings:
-//                        return true;
-//                    default:
-//                        return false;
-//                }
-//            }
-//
-//            @Override
-//            public void onDestroyActionMode(ActionMode actionMode) {
-//
 //            }
 //        });
 
-        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        return rootView;
+    }
+
+    private void playFile(File file) {
+        mPlayer = new MediaPlayer();
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                final int index = i;
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setTitle("Delete")
-                        .setMessage("Are you sure you want to delete " + mData.get(i) + "?")
-                        .setCancelable(false)
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/JustRecord/" + mData.get(index)).delete();
-                                refreshListView();
-                                dialogInterface.cancel();
-                            }
-                        })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                dialogInterface.cancel();
-                            }
-                        });
-                builder.create().show();
-                return true;
+            public void onCompletion(MediaPlayer mp) {
+                stopPlayback();
             }
         });
 
-        return rootView;
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        try {
+            mPlayer.setDataSource(file.getPath());
+            mPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mPlayer.start();
+        mPlayerState = MediaPlayerState.PLAYING;
+    }
+
+    private void pausePlayback() {
+        mPlayer.pause();
+        mPlayerState = MediaPlayerState.PAUSED;
+    }
+
+    private void resumePlayback() {
+        if(mPlayer != null) {
+            mPlayer.start();
+            mPlayerState = MediaPlayerState.PLAYING;
+        } else {
+            mPlayerState = MediaPlayerState.STOPPED;
+        }
+    }
+
+    private void stopPlayback() {
+        if(mPlayer != null) {
+            mPlayer.stop();
+            mPlayer.release();
+            mPlayer = null;
+        }
+        mPlayerState = MediaPlayerState.STOPPED;
     }
 
     private void savedFilterButtonTap (View btn) {
@@ -199,8 +249,6 @@ public class SavedFragment extends Fragment {
             item.setSelected(false);
         }
         btn.setSelected(true);
-
-
     }
 
     private ArrayList<File> getListFiles(File parentDir) {
@@ -243,8 +291,38 @@ public class SavedFragment extends Fragment {
         });
     }
 
-    private void sortByLength() {
+    private void sortByFileType() {
+        Collections.sort(mData, new Comparator<File>() {
+            public int compare(File a, File b) {
+                return a.getName().split(".")[1].compareTo(b.getName().split(".")[1]);
+            }
+        });
+    }
 
+    private void sortByLength() {
+        Collections.sort(mData, new Comparator<File>() {
+            public int compare(File a, File b) {
+                long aLength = getAudioFileDuration(a);
+                long bLength = getAudioFileDuration(b);
+                if(aLength > bLength) return 1;
+                else if(aLength < bLength) return -1;
+                else return 0;
+            }
+        });
+    }
+
+    private long getAudioFileDuration(File file) {
+        MediaPlayer player = new MediaPlayer();
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        try {
+            player.setDataSource(file.getPath());
+            player.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return player.getDuration();
     }
 
     private void sortBySize() {
